0c61cb242a1fc9fd2756ae9bde309e29cb7b5d02
tim9800 <92989679+tim9800@users.noreply.github.com>
2024-10-15 00:11:18 +1100

    Solve Ex02: Write and test function that repeats a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
 
+    const i = 0;
+    let out = '';
+
+    while (num > 0) {
+        out += string;
+        num--;
+    }
+
+    return out;
 };
 
 // Do not edit below this line
