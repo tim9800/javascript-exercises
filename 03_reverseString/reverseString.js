d4c1445c9eb70e910a1b2c9ce0aadaf439ddf1ff
tim9800 <92989679+tim9800@users.noreply.github.com>
2024-10-15 00:24:47 +1100

    Solve Ex03: Write and test function that reverses strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringArray = string.split("");
+    const length = stringArray.length;
 
+    const stringArrayReversed = new Array(length);
+    for (let i = 0; i < length; i++) {
+        stringArrayReversed[i] = stringArray[length - i - 1];
+    }
+
+    let stringReversed = stringArrayReversed.join("");
+
+    return(stringReversed);
 };
 
 // Do not edit below this line
