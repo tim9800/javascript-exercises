21bc7c42407f208a3cbdf20e42370679c052331f
tim9800 <92989679+tim9800@users.noreply.github.com>
2024-10-15 20:02:17 +1100

    Ex05: Create function that sums all integers between two integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    if ((typeof n1 != 'number') || (typeof n2 != 'number')) {
+        return 'ERROR';
+    };
 
+    if (!(Number.isInteger(n1) && Number.isInteger(n2))) {
+        return 'ERROR';
+    };
+
+    if (n1 < 0 || n2 < 0) {
+        return 'ERROR';
+    }
+
+    let start = n1;
+    let end = n2;
+    if (n1 > n2) {
+        start = n2;
+        end = n1;
+    }
+
+    let count = 0;
+    for (let i = start; i <= end; i++) {
+        count += i;
+    };
+
+    return(count)
 };
 
 // Do not edit below this line
