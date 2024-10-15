470e0f75696c0bc2e3b92b49a8254181b4e96104
tim9800 <92989679+tim9800@users.noreply.github.com>
2024-10-15 19:10:12 +1100

    Ex04: Draft function that removes elements from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+    
+    for (const elem1 in remove) {
+        for (let i = 0; i < arr.length; i++) {
+            if (elem1 == arr[i]) {
+                arr.splice(i, 1);
+            }
+        }
+     }
+
+     return(arr)
 };
 
 // Do not edit below this line
