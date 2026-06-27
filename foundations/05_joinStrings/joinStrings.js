3999147b6121b8f2b69f7cfdfe8ef944ccb79ed7
Timothy Wong <92989679+tim9800@users.noreply.github.com>
2026-06-27 19:47:59 +1000

    Javascript revision

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -6,6 +6,15 @@
 
  The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
+
+const fullName = `${firstName} ${lastName}`;
+const age = thisYear - birthYear;
+
+const greeting = `Hello! My name is ${fullName} and I am ${age} years old.`; 
 
 // Add your code right below, good luck!
 
